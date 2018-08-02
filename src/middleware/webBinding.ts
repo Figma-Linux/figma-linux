8cc73b1a7bd0a3aab67219f5ffce1dc0d2b20618
Chugunov Roman <Chugunovrv@lad24.ru>
2018-08-02 16:21:41 +0300

    Fixes #7 - the export images

--- src/middleware/webBinding.ts
+++ src/middleware/webBinding.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../@types/renderer/index.d.ts" />
 
 import * as E from "electron";
+import * as path from "path";
+import * as fs from "fs";
 
 import { sendMsgToMain } from "./util";
 
@@ -15,8 +17,8 @@ const onWebMessage = (event: MessageEvent) => {
     let resultPromise = undefined;
 
     try {
-        resultPromise = msg.name && publicAPI[msg.name](msg.args);
-        console.log('onWebMessage, msg: ', msg);
+        console.log('onWebMessage, publicAPI, msg: ', publicAPI, msg);
+        resultPromise = msg.name && publicAPI && publicAPI[msg.name](msg.args);
     }
     finally {
         if (msg.promiseID != null) {
@@ -109,6 +111,120 @@ const initWebApi = (version: number, fileBrowser: boolean) => {
 const publicAPI: any = {
     setTitle(args: any) {
         sendMsgToMain('setTitle', args.title);
+    },
+
+    writeFiles(args: any) {
+        console.log('writeFiles args: ', args);
+        const files = args.files;
+        if (!Array.isArray(files) || files.length === 0) {
+            return;
+        }
+        let skipReplaceConfirmation = false;
+        let directoryPath;
+        if (files.length === 1 && !files[0].name.includes(path.sep)) {
+            const originalFileName = files[0].name;
+            const savePath = E.remote.dialog.showSaveDialog({
+                defaultPath: path.basename(originalFileName),
+                showsTagField: false,
+            });
+            if (savePath) {
+                directoryPath = path.dirname(savePath);
+                files[0].name = path.basename(savePath);
+                if (path.extname(files[0].name) === '') {
+                    files[0].name += path.extname(originalFileName);
+                }
+                else {
+                    skipReplaceConfirmation = true;
+                }
+            }
+        }
+        else {
+            const directories = E.remote.dialog.showOpenDialog({
+                properties: ['openDirectory', 'createDirectory'],
+                buttonLabel: 'Save',
+            });
+            if (!directories || directories.length !== 1) {
+                return;
+            }
+            directoryPath = directories[0];
+        }
+        if (!directoryPath) {
+            return;
+        }
+        directoryPath = path.resolve(directoryPath);
+        let filesToBeReplaced = 0;
+        for (let file of files) {
+            const outputPath = path.join(directoryPath, file.name);
+            const validExtensions = [
+                '.fig',
+                '.jpg',
+                '.pdf',
+                '.png',
+                '.svg',
+            ];
+            if (path.relative(directoryPath, outputPath).startsWith('..') ||
+                !validExtensions.findIndex(i => i === path.extname(outputPath))) {
+                E.remote.dialog.showMessageBox({
+                    type: 'error',
+                    title: 'Export Failed',
+                    message: 'Export failed',
+                    detail: `"${outputPath}" is not a valid path. No files were saved.`,
+                    buttons: ['OK'],
+                    defaultId: 0,
+                });
+                return;
+            }
+            try {
+                fs.accessSync(outputPath, fs.constants.R_OK);
+                ++filesToBeReplaced;
+            }
+            catch (ex) {
+            }
+        }
+        if (filesToBeReplaced > 0 && !skipReplaceConfirmation) {
+            const single = filesToBeReplaced === 1;
+            const selectedID = E.remote.dialog.showMessageBox({
+                type: 'warning',
+                title: 'Replace Existing Files',
+                message: `Replace existing file${single ? '' : `s`}?`,
+                detail: `${single ? `"${files[0].name}" already exists` : `${filesToBeReplaced} files including "${files[0].name}" already exist`}. Replacing ${single ? 'it' : 'them'} will overwrite ${single ? 'its' : 'their'} existing contents.`,
+                buttons: ['Replace', 'Cancel'],
+                defaultId: 0,
+            });
+            if (selectedID !== 0) {
+                return;
+            }
+        }
+        for (let file of files) {
+            {
+                const parts = file.name.split('/');
+                parts.pop();
+                let dirPath = directoryPath;
+                for (let part of parts) {
+                    try {
+                        dirPath = path.join(dirPath, part);
+                        fs.mkdirSync(dirPath);
+                    }
+                    catch (ex) {
+                    }
+                }
+            }
+            try {
+                const outputPath = path.join(directoryPath, file.name);
+                const opts = { encoding: 'binary' };
+                fs.writeFileSync(outputPath, Buffer.from(file.buffer), opts);
+            }
+            catch (ex) {
+                E.remote.dialog.showMessageBox({
+                    type: 'error',
+                    title: 'Export Failed',
+                    message: 'Saving file failed',
+                    detail: `"${file.name}" could not be saved. Remaining files will not be saved.`,
+                    buttons: ['OK'],
+                    defaultId: 0,
+                });
+            }
+        }
     }
 }
 

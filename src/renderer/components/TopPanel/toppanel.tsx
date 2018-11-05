1de544e4795869464d1cff3aa907aba615c4bdf8
Chugunov Roman <Zebs-BMK@yandex.ru>
2018-11-05 20:46:37 +0300

    Updated styles

--- src/renderer/components/TopPanel/toppanel.tsx
+++ src/renderer/components/TopPanel/toppanel.tsx
@@ -31,7 +31,7 @@ const TopPanel: React.SFC<TopPanelProps> = (props) => {
                         </svg>
                     </div> : null
                 }
-                <div className="newTab" onClick={props.newTab}>
+                <div className="button newTab" onClick={props.newTab}>
                     <svg className="svg" width="14" height="14" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2.5 6.5v1h4v4h1v-4h4v-1h-4v-4h-1v4h-4z" fill="#fff"></path>
                     </svg>

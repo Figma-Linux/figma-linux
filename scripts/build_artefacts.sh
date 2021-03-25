02094613163f044477639856a5118bf59305f478
Chugunov Roman <Zebs-BMK@yandex.ru>
2021-03-25 16:32:36 +0300

    fix: workflow

--- scripts/build_artefacts.sh
+++ scripts/build_artefacts.sh
@@ -13,7 +13,7 @@ rm -rf ./build/installers
 mkdir -p ./build/installers
 
 # copy artefacts from image
-docker run -v $PWD/build:/opt/mount --rm -ti figma-linux-artefacts:latest bash -c "cp -r /usr/src/figma-linux/build/installers /opt/mount && chown 1000:1000 /opt/mount -R"
+# docker run -v $PWD/build:/opt/mount --rm -ti figma-linux-artefacts:latest bash -c "cp -r /usr/src/figma-linux/build/installers /opt/mount && chown 1000:1000 /opt/mount -R"
 
-docker image rm figma-linux-artefacts
+# docker image rm figma-linux-artefacts
 

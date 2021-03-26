de49e0e86227da536428ad6474188736a3ae9f06
Chugunov Roman <Zebs-BMK@yandex.ru>
2021-03-26 18:14:50 +0300

    other: update generate_release_notes.pl script

--- scripts/generate_release_notes.pl
+++ scripts/generate_release_notes.pl
@@ -2,10 +2,13 @@
 
 my $features=`git log \$(git tag | tail -n2 | head -n1)..HEAD --no-merges --oneline | grep -Eo "feat:.*" | uniq`;
 my $fixes=`git log \$(git tag | tail -n2 | head -n1)..HEAD --no-merges --oneline | grep -Eo "fix:.*" | uniq`;
+my $other=`git log \$(git tag | tail -n2 | head -n1)..HEAD --no-merges --oneline | grep -Eo "other:.*" | uniq`;
 my $hasFeatures=`printf "$features" | wc -l | tr -d '\n'`;
 my $hasFixes=`printf "$fixes" | wc -l | tr -d '\n'`;
+my $hasOther=`printf "$other" | wc -l | tr -d '\n'`;
 my @featureList = split /\n/, $features;
 my @fixList = split /\n/, $fixes;
+my @otherList = split /\n/, $other;
 my $baseUrl = "https://github.com/Figma-Linux/figma-linux/issues";
 my $release_note_file_path = "./release_notes";
 
@@ -37,6 +40,27 @@ if ($hasFixes > 0) {
   `echo '## Bug Fixes:' >> $release_note_file_path`;
 
   for my $msg (@fixList) {
+    my $issue = `echo "$msg" | grep -Eo "#.*" | tr -d '\n'`;
+
+    if ($issue ne "") {
+      my $issueId = substr $issue, 1;
+      $msg =~ s/ ?(Close|#).*//gi;
+
+      `echo "* $msg [$issue]($baseUrl/$issueId)" >> $release_note_file_path`;
+    } else {
+      `echo "* $msg" >> $release_note_file_path`;
+    }
+  }
+
+  if ($hasOther > 0) {
+    `echo '' >> $release_note_file_path`;
+  }
+}
+
+if ($hasOther > 0) {
+  `echo '## Other Changes:' >> $release_note_file_path`;
+
+  for my $msg (@otherList) {
     my $issue = `echo "$msg" | grep -Eo "#.*" | tr -d '\n'`;
 
     if ($issue ne "") {

5af20627788ceb4ca63a7a570cd08e3501871e2e
Chugunov Roman <Zebs-BMK@yandex.ru>
2021-03-26 18:12:40 +0300

    other: added script for bump figma linux version

---
+++ scripts/bump_version.pl
@@ -0,0 +1,20 @@
+#!/bin/perl
+
+my $version = "$ARGV[0]";
+
+if ($version eq "") {
+  print "Need pass version.\n";
+  exit 1;
+}
+
+my $prevVersion = `git tag | tail -n1 | tr -d 'v' | tr -d '\n'`;
+
+printf("Bump %s to %s version.\n", $prevVersion, $version);
+
+system("sed -i \"s/$prevVersion/$version/\" ./package.json");
+system("sed -i \"s/$prevVersion/$version/\" ./src/package.json");
+system("sed -i \"s/$prevVersion/$version/\" ./snap/snapcraft.yaml");
+
+system("git add .");
+system("git commit -m 'Release v$version'");
+system("git tag -a v$version -m 'Publish v$version release'");

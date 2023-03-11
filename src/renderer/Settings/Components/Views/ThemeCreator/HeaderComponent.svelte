743868a6e54fe35d99bd7847e1bf1b0b35038646
Chugunov Roman <Zebs-BMK@yandex.ru>
2023-03-11 23:26:12 +0300

    impr: plans to add buttons in ThemeCreator header

--- src/renderer/Settings/Components/Views/ThemeCreator/HeaderComponent.svelte
+++ src/renderer/Settings/Components/Views/ThemeCreator/HeaderComponent.svelte
@@ -2,19 +2,25 @@
   import { ipcRenderer } from "electron";
   import { SecondaryButton, Flex } from "Common";
 
-  import { themesLoaded } from "../../../store";
-
+  function onReset(event: MouseEvent) {
+    ipcRenderer.send("onReset");
+  }
+  function onSave(event: MouseEvent) {
+    ipcRenderer.send("onSave");
+  }
   function onAddToThemes(event: MouseEvent) {
-    ipcRenderer.send("syncThemes");
-    themesLoaded.set(false);
+    ipcRenderer.send("onAddToThemes");
   }
   function onExport(event: MouseEvent) {
-    ipcRenderer.send("syncThemes");
-    themesLoaded.set(false);
+    ipcRenderer.send("onExport");
   }
 </script>
 
 <Flex>
+  <SecondaryButton height="28px">Reset</SecondaryButton>
+  <Flex width="10px" />
+  <SecondaryButton height="28px">Save</SecondaryButton>
+  <Flex width="10px" />
   <SecondaryButton height="28px">Add to themes</SecondaryButton>
   <Flex width="10px" />
   <SecondaryButton height="28px">Export</SecondaryButton>
